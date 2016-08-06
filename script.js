98f6cbeff776f84fc9fef148809b6cb06c367d70
Michael Cruz <michael.cruz@uwalumni.com>
2016-08-06 14:48:33 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function readMouseMove(e){
+	var result_x = document.getElementById('x_result');
+	var result_y = document.getElementById('y_result');
+	result_x.innerHTML = e.clientX;
+	result_y.innerHTML = e.clientY;
+}
+document.onmousemove = readMouseMove;
