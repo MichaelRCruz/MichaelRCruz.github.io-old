3cdb10380c422953df41f155d2a6fe2424a86d39
Michael Cruz <michael.cruz@uwalumni.com>
2016-08-12 15:30:11 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-function readMouseMove(e){
-	var result_x = document.getElementById('x_result');
-	var result_y = document.getElementById('y_result');
-	result_x.innerHTML = e.clientX;
-	result_y.innerHTML = e.clientY;
-}
-document.onmousemove = readMouseMove;
+// background color interaction with the "sun"
+var _left, _top, _leftTop;
+$(document).ready(function() {
+	$('#sun').draggable({
+		drag: function(){
+			var $sunPos = $('#sun');
+			var $offset = $sunPos.offset();
+			_left = $offset.left;
+			_top = $offset.top;
+			$sunPos.html ( "left: " + _left + ", top: " + _top );
+			var $width = ($(document).width())/255;
+			var $height = ($(document).height())/255;
+			var $winLeft = parseInt(_left/$width, 10);
+			var $winTop = parseInt(_top/$height, 10);
+			var $winLeftTop = (-($winLeft+($winTop*1.5))/2)+255;
+			_leftTop = $winLeftTop;
+			$("#above").css("background-color", "rgb(150,"+_leftTop+","+_leftTop+")");
+			}
+	});
+});
